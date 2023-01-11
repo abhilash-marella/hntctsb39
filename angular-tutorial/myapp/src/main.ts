a8ecc319cf41389bc27ec3e6eda58ea3f30110a2
Arun Kumar <arun.kumar@iiht.co.in>
2023-01-11 13:03:49 +0530

    angular, day2

--- angular-tutorial/myapp/src/main.ts
+++ angular-tutorial/myapp/src/main.ts
@@ -6,7 +6,12 @@ import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
 // import { AppModule } from "./04-directives-attribute/app.module";
 // import { AppModule } from "./05-directives-structral/app.module";
 // import { AppModule } from "./06-di/app.module";
-import { AppModule } from "./07-http/app.module";
+// import { AppModule } from "./07-http/app.module";
+// import { AppModule } from "./08-pipes/app.module";
+// import { AppModule } from "./09-routing/app.module";
+// import { AppModule } from "./10-nestModules/app.module";
+// import { AppModule } from "./11-route-params/app.module";
+import { AppModule } from "./12-form/app.module";
 
 platformBrowserDynamic()
   .bootstrapModule(AppModule)
